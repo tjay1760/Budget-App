2e844446c87c6b91b901cff914b9a7b6807ec528
tjay1760 <mcjthiongo@gmail.com>
2023-10-16 23:39:58 +0300

    Update category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,8 +13,38 @@ class CategoriesController < ApplicationController
   def new
     @user = current_user
     @category = Category.new
+    if @category.save
+      format.html {}
+      format.json {}
+    else
+      format.html { render :new, status: :unprocessable_entity }
+    end
   end
 
-  def create; end
-  def destroy; end
+  def create
+    @user = current_user
+    @category = @user.categories.new(category_params)
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    respond_to do |format|
+      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
