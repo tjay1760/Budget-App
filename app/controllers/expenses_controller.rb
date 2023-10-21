41c26ce203eb02ca9a52f77ccfe0a5a64df94b33
tjay1760 <mcjthiongo@gmail.com>
2023-10-21 16:43:07 +0300

    remove bug causing double save

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -79,7 +79,7 @@ class ExpensesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def expense_params
-    params.require(:expense).permit(:name, :amount, :author_id, category_ids:[])
+    params.require(:expense).permit(:name, :amount, :author_id)
   end
 
   def save?(saved_expenses, category_ids)
